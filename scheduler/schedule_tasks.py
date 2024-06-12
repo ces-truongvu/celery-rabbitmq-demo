1558a47f1e91d9bc3d198cd8383cf9a4f2f757fd
Truong Vu <truong.vu@codeenginestudio.com>
2024-06-12 16:08:29 +0700

    init

--- scheduler/schedule_tasks.py
+++ scheduler/schedule_tasks.py
@@ -1,27 +1,43 @@
+
 import pika
 import json
 import os
+import uuid
 
-rabbitmq_url = os.getenv('RABBITMQ_URL', 'amqp://user:password@localhost:5672/')
-queue_name = 'tasks'
+rabbitmq_url = os.getenv('RABBITMQ_URL', 'amqp://user:password@rabbitmq:5672//')
 
-connection = pika.BlockingConnection(pika.URLParameters(rabbitmq_url))
-channel = connection.channel()
+queue_name = 'tasks'  # Use the default Celery queue
 
-channel.queue_declare(queue=queue_name, durable=True)
+try:
+    connection = pika.BlockingConnection(pika.URLParameters(rabbitmq_url))
+    channel = connection.channel()
 
-task = {
-    'url': 'https://google.com',
-    'screenshot_path': 'google.png'
-}
+    channel.queue_declare(queue=queue_name, durable=True)
 
-channel.basic_publish(
-    exchange='',
-    routing_key=queue_name,
-    body=json.dumps(task),
-    properties=pika.BasicProperties(
-        delivery_mode=2,  # make message persistent
-    ))
+    task = {
+        'url': 'https://google1.com',
+        'screenshot_path': 'google.png'
+    }
 
-print(f" [x] Sent {task}")
-connection.close()
+    message = {
+        'task': 'tasks.run_visual_regression_test',
+        'id': str(uuid.uuid4()),
+        'args': [task],
+        'kwargs': {},
+        'retries': 1,
+        'eta': None
+    }
+
+    channel.basic_publish(
+        exchange='',
+        routing_key=queue_name,
+        body=json.dumps(message),
+        properties=pika.BasicProperties(
+            content_type='application/json',
+            delivery_mode=2,  # make message persistent
+        ))
+    print(f" [x] Sent {task}")
+
+    connection.close()
+except Exception as e:
+    print(f"Error: {e}")
--- worker/tasks.py
+++ worker/tasks.py
@@ -2,18 +2,20 @@ from celery import Celery
 from playwright.sync_api import sync_playwright
 import os
 
-rabbitmq_url = os.getenv('RABBITMQ_URL', 'amqp://user:password@localhost:5672/')
+rabbitmq_url = os.getenv('RABBITMQ_URL', 'pyamqp://user:password@rabbitmq:5672//')
 
 app = Celery('tasks', broker=rabbitmq_url)
 
 @app.task
 def run_visual_regression_test(test_config):
     with sync_playwright() as p:
+        print(f" [x] Config: {test_config}")
         browser = p.chromium.launch()
         page = browser.new_page()
         
         # Example test: navigate to a page and take a screenshot
         page.goto(test_config['url'])
+
         page.screenshot(path=test_config['screenshot_path'])
         
         browser.close()
